7b55b29706b6ae8bb04b21ee0864ff429b7e2366
Jhone Oliver <jhone@oliverdigital.com.br>
2017-11-28 18:22:14 -0200

    Finalizado o CRUD de usuários, na Oliver

--- index.php
+++ index.php
@@ -65,6 +65,11 @@ $app->get("/admin/users", function() {
 
 $app->get("/admin/users/:iduser/delete", function($iduser){
   User::verifyLogin(); 
+  $user = new User();
+  $user->get((int)$iduser);
+  $user->delete();
+  header("Location: /admin/users");
+  exit();
 });
 
 
@@ -100,6 +105,15 @@ $app->post("/admin/users/create", function(){
 
 $app->post("/admin/users/:iduser", function($iduser){
   User::verifyLogin(); 
+
+  $user = new User();
+   $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+  $user->get((int)$iduser);
+  $user->setData($_POST);
+  $user->update();
+  header("Location: /admin/users");
+  exit();
+
 });
 
 
