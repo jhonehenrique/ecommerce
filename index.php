52bfce45c532b071d6064069380f49afbf5cb7cf
Jhone Oliver <jhone@oliverdigital.com.br>
2018-01-15 21:41:38 -0200

    Categorias

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\user;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -173,6 +174,18 @@ $app->post("/admin/forgot/reset", function(){
       "footer"=>false
       ]);
     $page->setTpl("forgot-reset-success");
+
+});
+
+
+$app->get("/admin/categories", function(){
+  
+   $categories = Category::listAll();
+
+   $page = new PageAdmin([
+      ]);
+    $page->setTpl("categories", ['categories'=>$categories]);
+
 
 });
 
