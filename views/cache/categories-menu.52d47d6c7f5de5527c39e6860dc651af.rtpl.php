ab8d09a9e7f35d5251bdbe75497a6a75d97ba240
Jhone Henrique <jhone.henrique.info@gmail.com>
2018-01-23 23:36:41 -0200

    merge

--- views/cache/categories-menu.52d47d6c7f5de5527c39e6860dc651af.rtpl.php
+++ views/cache/categories-menu.52d47d6c7f5de5527c39e6860dc651af.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Android</a></li><li><a href="/categories/7">Apple</a></li><li><a href="/categories/11">Motorola</a></li><li><a href="/categories/10">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Android</a></li><li><a href="/categories/7">Apple</a></li><li><a href="/categories/12">Jhone</a></li><li><a href="/categories/11">Motorola</a></li><li><a href="/categories/10">Samsung</a></li>
