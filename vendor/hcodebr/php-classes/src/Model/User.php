36bada164c6944af0634871715d0e589d1765152
Jhone Oliver <jhone@oliverdigital.com.br>
2018-02-21 10:21:42 -0300

    checkout na Oliver

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,6 +10,7 @@ class User extends Model{
 
 	const SESSION = "User";
 	const SECRET = "HcodePhp7_Secret";
+	const ERROR = "UserError";
 
 	public static function getFromSession() {
 		$user = new User();
@@ -240,6 +241,23 @@ class User extends Model{
             ":iduser"=>$this->getiduser()
         ));
     }
+
+ 	public static function setError($msg)
+	{
+		$_SESSION[User::ERROR] = $msg;
+	}
+	
+	public static function getError() {
+		$msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+		
+		User::clearError();
+		
+		return $msg;
+	}
+	
+	public static function clearError() {
+		$_SESSION[User::ERROR] = NULL;
+	}
 
 }
 
